cb13c5c9260ef90fb877b560a5a6bd6f30c9800a
Usiadlo <tercjak.dawid@gmail.com>
2024-05-16 11:34:47 +0200

    init

---
+++ src/main/java/com/steampromo/steamz/SteamzApplication.java
@@ -0,0 +1,13 @@
+package com.steampromo.steamz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SteamzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/steampromo/steamz/SteamzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steampromo.steamz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteamzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
