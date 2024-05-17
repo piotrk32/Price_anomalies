122de3a6e060ce5bffc0388f89d6a5db5d091d27
piotrk32 <piotrkepistyuwm@gmail.com>
2024-05-17 19:32:56 +0200

    Alert entity created

---
+++ src/main/java/com/steampromo/steamz/items/domain/alert/Alert.java
@@ -0,0 +1,36 @@
+package com.steampromo.steamz.items.domain.alert;
+
+
+import com.steampromo.steamz.items.domain.item.Item;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "alerts")
+@Setter
+@Getter
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Alert {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id", nullable = false)
+    private Item item;
+
+    @Column(name = "date", nullable = false)
+    private LocalDateTime date;
+
+    @Column(name = "price_gap", nullable = false)
+    private int priceGap;
+}
