c548a27233f77f5814351e58c934e4c3ee360b5d
piotrk32 <piotrkepistyuwm@gmail.com>
2024-05-21 16:37:06 +0200

    findByCategory method added

--- src/main/java/com/steampromo/steamz/items/repository/ItemRepository.java
+++ src/main/java/com/steampromo/steamz/items/repository/ItemRepository.java
@@ -1,9 +1,12 @@
 package com.steampromo.steamz.items.repository;
 
 import com.steampromo.steamz.items.domain.item.Item;
+import com.steampromo.steamz.items.domain.item.enums.CategoryEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ItemRepository extends JpaRepository<Item, UUID> {
+    List<Item> findByCategory(CategoryEnum category);
 }
