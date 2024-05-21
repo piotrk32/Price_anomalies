20920d0d13d5a63aa13446fa0e63ef7a81038e5d
piotrk32 <piotrkepistyuwm@gmail.com>
2024-05-21 16:31:35 +0200

    createAlert method added

--- src/main/java/com/steampromo/steamz/items/service/item/ItemService.java
+++ src/main/java/com/steampromo/steamz/items/service/item/ItemService.java
@@ -1,9 +1,13 @@
 package com.steampromo.steamz.items.service.item;
 
+import com.steampromo.steamz.items.domain.alert.Alert;
+import com.steampromo.steamz.items.domain.item.Item;
 import com.steampromo.steamz.items.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -15,6 +19,14 @@ public class ItemService {
     private boolean isAnomaly(double latestPrice, double medianPrice) {
         double priceGap = Math.abs(latestPrice - medianPrice);
         return (priceGap / medianPrice) > 0.1;
+    }
+
+    private void createAlert(Item item, double latestPrice, double medianPrice) {
+        Alert alert = new Alert();
+        alert.setItem(item);
+        alert.setDate(LocalDateTime.now());
+        alert.setPriceGap((int) ((latestPrice - medianPrice) * 100));
+        item.getAlerts().add(alert);
     }
 
 
