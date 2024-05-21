aed8244ec4952e392900ecd4ae44e8c95726b2d9
piotrk32 <piotrkepistyuwm@gmail.com>
2024-05-21 16:29:24 +0200

    dependencies injected

--- src/main/java/com/steampromo/steamz/items/service/item/ItemService.java
+++ src/main/java/com/steampromo/steamz/items/service/item/ItemService.java
@@ -1,11 +1,16 @@
 package com.steampromo.steamz.items.service.item;
 
+import com.steampromo.steamz.items.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ItemService {
+
+    private final ItemRepository itemRepository;
+    private JavaMailSender mailSender;
 
 
 }
