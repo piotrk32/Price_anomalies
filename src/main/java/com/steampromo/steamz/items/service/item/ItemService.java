8d1128f056d31b41493a197c89187a045f0c8427
piotrk32 <piotrkepistyuwm@gmail.com>
2024-05-21 16:28:39 +0200

    Service initialized

---
+++ src/main/java/com/steampromo/steamz/items/service/item/ItemService.java
@@ -0,0 +1,11 @@
+package com.steampromo.steamz.items.service.item;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ItemService {
+
+
+}
