45983b5460a25e01e9c376898ceaebbfe7f93af9
piotrk32 <piotrkepistyuwm@gmail.com>
2024-05-23 17:31:26 +0200

    Logging added

--- src/main/java/com/steampromo/steamz/items/exception/CustomResponseErrorHandler.java
+++ src/main/java/com/steampromo/steamz/items/exception/CustomResponseErrorHandler.java
@@ -2,11 +2,13 @@ package com.steampromo.steamz.items.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.client.DefaultResponseErrorHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class CustomResponseErrorHandler extends DefaultResponseErrorHandler {
 
@@ -14,19 +16,22 @@ public class CustomResponseErrorHandler extends DefaultResponseErrorHandler {
 
     @Override
     public void handleError(ClientHttpResponse response) throws IOException {
+        String body = StreamUtils.copyToString(response.getBody(), StandardCharsets.UTF_8);
         HttpStatus statusCode = HttpStatus.resolve(response.getStatusCode().value());
+
         if (statusCode != null) {
-            if (statusCode.is5xxServerError()) {
-                logger.error("Server error: " + statusCode + ", " + response.getStatusText());
-            } else if (statusCode.is4xxClientError()) {
+            if (statusCode.is4xxClientError()) {
                 if (statusCode == HttpStatus.NOT_FOUND) {
-                    logger.error("Error 404: Not Found");
+                    logger.error("Error 404: Not Found - {}", body);
                 } else {
-                    logger.error("Client error: " + statusCode + ", " + response.getStatusText());
+                    logger.error("Client error: {} {}, Body: {}", statusCode, response.getStatusText(), body);
                 }
+            } else if (statusCode.is5xxServerError()) {
+                logger.error("Server error: {} {}, Body: {}", statusCode, response.getStatusText(), body);
             }
         } else {
-            logger.error("Unknown status code: " + response.getStatusCode() + ", " + response.getStatusText());
+            logger.error("Unknown status code: {}, Body: {}", response.getStatusCode(), body);
         }
     }
+
 }
