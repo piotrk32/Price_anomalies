6efc65629da3469d54f248c599b2fd98c06d7f8c
piotrk32 <piotrkepistyuwm@gmail.com>
2024-05-24 14:46:47 +0200

    MailConfig changed to read values from yml

--- src/main/java/com/steampromo/steamz/alerts/configuration/MailConfig.java
+++ src/main/java/com/steampromo/steamz/alerts/configuration/MailConfig.java
@@ -1,5 +1,6 @@
 package com.steampromo.steamz.alerts.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -10,19 +11,40 @@ import java.util.Properties;
 @Configuration
 public class MailConfig {
 
+    @Value("${mail.host}")
+    private String host;
+
+    @Value("${mail.port}")
+    private int port;
+
+    @Value("${mail.username}")
+    private String username;
+
+    @Value("${mail.password}")
+    private String password;
+
+    @Value("${mail.smtp.auth}")
+    private boolean smtpAuth;
+
+    @Value("${mail.smtp.starttls-enable}")
+    private boolean startTlsEnable;
+
+    @Value("${mail.smtp.debug}")
+    private boolean mailDebug;
+
     @Bean
     public JavaMailSender javaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("poczta.o2.pl");
-        mailSender.setPort(587);
-        mailSender.setUsername("piotrk322@o2.pl");
-        mailSender.setPassword("Dupacyce1");
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "true");
+        props.put("mail.smtp.auth", Boolean.toString(smtpAuth));
+        props.put("mail.smtp.starttls.enable", Boolean.toString(startTlsEnable));
+        props.put("mail.debug", Boolean.toString(mailDebug));
 
         return mailSender;
     }
