782187e2e77afb332fa43a91a8b609ad73247844
piotrk32 <piotrkepistyuwm@gmail.com>
2024-05-24 13:46:24 +0200

    useless methods removed

--- src/main/java/com/steampromo/steamz/items/service/ItemService.java
+++ src/main/java/com/steampromo/steamz/items/service/ItemService.java
@@ -156,30 +156,5 @@ public class ItemService {
         mailSender.send(message);
     }
 
-//    public void save(Item item) {
-//        String sql = "INSERT INTO items (id, item_name, lowest_price, median_price, category) VALUES (?, ?, ?, ?, ?)";
-//        jdbcTemplate.update(sql, item.getId(), item.getItemName(), item.getLowestPrice(), item.getMedianPrice(), item.getCategory().name());
-//    }
-//
-//    public List<Item> findAll() {
-//        String sql = "SELECT * FROM items";
-//        return jdbcTemplate.query(sql, this::mapRowToItem);
-//    }
-
-//    public Item mapRowToItem(ResultSet rs, int rowNum) throws SQLException {
-//        Item item = new Item();
-//        item.setId(UUID.fromString(rs.getString("id")));
-//        item.setItemName(rs.getString("item_name"));
-//        item.setLowestPrice(rs.getDouble("lowest_price"));
-//        item.setMedianPrice(rs.getDouble("median_price"));
-//        item.setCategory(CategoryEnum.valueOf(rs.getString("category")));
-//        return item;
-//    }
-
-
-
-
-
-
 }
 
