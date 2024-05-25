fe3bd84f147cf468af8ca4c88cf71df4b091832f
piotrk32 <piotrkepistyuwm@gmail.com>
2024-05-25 18:24:55 +0200

    MailConfig changed

--- src/main/java/com/steampromo/steamz/alerts/configuration/MailConfig.java
+++ src/main/java/com/steampromo/steamz/alerts/configuration/MailConfig.java
@@ -1,51 +1,19 @@
 package com.steampromo.steamz.alerts.configuration;
 
+import com.sendgrid.SendGrid;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 
-import java.util.Properties;
 
 @Configuration
 public class MailConfig {
 
-    @Value("${mail.host}")
-    private String host;
-
-    @Value("${mail.port}")
-    private int port;
-
-    @Value("${mail.username}")
-    private String username;
-
-    @Value("${mail.password}")
-    private String password;
-
-    @Value("${mail.smtp.auth}")
-    private boolean smtpAuth;
-
-    @Value("${mail.smtp.starttls-enable}")
-    private boolean startTlsEnable;
-
-    @Value("${mail.smtp.debug}")
-    private boolean mailDebug;
+    @Value("${sendgrid.api-key}")
+    private String sendGridApiKey;
 
     @Bean
-    public JavaMailSender javaMailSender() {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost(host);
-        mailSender.setPort(port);
-        mailSender.setUsername(username);
-        mailSender.setPassword(password);
-
-        Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", "smtp");
-        props.put("mail.smtp.auth", Boolean.toString(smtpAuth));
-        props.put("mail.smtp.starttls.enable", Boolean.toString(startTlsEnable));
-        props.put("mail.debug", Boolean.toString(mailDebug));
-
-        return mailSender;
+    public SendGrid sendGrid() {
+        return new SendGrid(sendGridApiKey);
     }
 }
