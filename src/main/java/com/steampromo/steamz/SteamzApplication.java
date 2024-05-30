6227407a46b240f93505d6b80502ef5cafddf264
piotrk32 <piotrkepistyuwm@gmail.com>
2024-05-30 21:07:11 +0200

    annotation added

--- src/main/java/com/steampromo/steamz/SteamzApplication.java
+++ src/main/java/com/steampromo/steamz/SteamzApplication.java
@@ -2,10 +2,13 @@ package com.steampromo.steamz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@ConfigurationPropertiesScan
+
 public class SteamzApplication {
 
     public static void main(String[] args) {
